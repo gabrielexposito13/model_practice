a3814e604677e54654ba43454a81467384717df8
gabrielexposito13 <ge1316a@american.edu>
2023-06-07 18:12:55 -0400

    Update Accuracy Practice.py

--- Accuracy Practice.py
+++ Accuracy Practice.py
@@ -2,54 +2,55 @@ import random
 import simpy
 import numpy as np
 
-DEER = 40
-DISTANCE = 100
-EVASION_CHANCE = 0.85
-HIT_CHANCE = 0.60
-SIM_TIME = 100
+NUM_SURVIVORS = 5 #objects
+ACCURACY = .03  #probability
+SHOTS_PER_MINUTE = 25 #per minute
+ZOMBIE_ATTACK_INTERVAL = 10 #per minute
 
-deer_hunted = 0
+SIM_TIME = 120 #minutes
 
-class Hunter:
-    def __innit__(self, env, deer, evasion_chance):
+zombies_killed = 0
+
+class Apocalypse:
+
+    def __init__(self, env, num_survivors, accuracy, shots_per_minute):
         self.env = env
-        self.deer = simpy.Resource(env, deer)
-        self.evasion_chance = evasion_chance
+        self.team = simpy.Resource(env, num_survivors)
+        self.accuracy = accuracy
+        self.shots_per_minute = shots_per_minute
 
-    def attack(self, env, hunter, hit_chance):
-        random_time = max(1, np.random.normal(self.evasion_chance, 0.60))
+    def defense(self, zombie):
+        random_time = max(1, np.random.normal(self.shots_per_minute, 10))
         yield self.env.timeout(random_time)
-        self.env = env
-        self.sam = simpy.Resource(env, hunter)
-        self.hit_chance = hit_chance
-    
-def scenario(env, engagement):
-    global deer_hunted
-    print(f"deer enters the zone.")
-    with engagement.deer() as request:
+        print(f"Dealt with {zombie} at {self.env.now:.2f}")
+
+def zombie(env, survivors):
+    global zombies_killed
+    print(f"Zombie appears at {env.now:.2f}!")
+    with survivors.team.request() as request:
         yield request
-        print(f"deer enters danger zone.")
-        yield env.process(engagement.attack)
-        print(f"deer has been hit")
-        deer_hunted += 1
+        print(f"Zombie engaged at {env.now:.2f}!")
+        yield env.process(survivors.support)
+        print(f"Zombie killed at {env.now:.2f}!")
+        zombies_killed += 1
 
-def setup(env, deer, evasion_chance, hit_chance):
-    engagement = Hunter(env, deer, evasion_chance, hit_chance)
+def setup(env, num_survivors, shots_per_minute, accuracy, zombie_attack_interval):
+    survivors = Apocalypse(env, num_survivors, shots_per_minute, accuracy, zombie_attack_interval)
 
-    for i in range(1 , 5):
-        env.process(engagement(env, i, engagement))
+    for i in range(1, 6):
+        env.process(zombie(env, i, survivors))
+    
+    while True:
+        yield env.timeout(random.randint(zombie_attack_interval -1, zombie_attack_interval +1 ))
+        i += 1
+        env.process(zombie(env, num_survivors, survivors))
 
-        while True:
-            yield env.timeout(random.randint(hit_chance -1, hit_chance +1))
-            i += 1
-            env.process(engagement(env, deer, engagement))
-
-print("Starting Simulation")
+print("Starting Apocalypse")
 env = simpy.Environment()
-env.process(setup(env, deer, EVASION_CHANCE, HIT_CHANCE))
-env.run(until1=SIM_TIME)
+env.process(setup(env, NUM_SURVIVORS, ACCURACY, SHOTS_PER_MINUTE, ZOMBIE_ATTACK_INTERVAL))
+env.run(until=SIM_TIME)
 
-print("Deer Hunted: " +str(deer_hunted))
+print("Zombies Killed: "  + str(zombies_killed))
 
 
 

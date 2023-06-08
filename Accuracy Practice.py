2a7ac24d5a96ccc0b511150c57e0bd16deafadb0
gabrielexposito13 <ge1316a@american.edu>
2023-06-08 11:08:00 -0400

    Update Accuracy Practice.py
    Working simulation

--- Accuracy Practice.py
+++ Accuracy Practice.py
@@ -1,70 +1,42 @@
-import numpy as np
-import simpy
 import random
 
-ROB_CLOSEST_APPROACH_TO_TIM = 101
+# Define constants
+tim_accuracy_max = 100
+tim_accuracy_decrease_per_meter_100_to_30 = 3
+tim_accuracy_decrease_per_meter_25_to_0 = 5
+tim_closest_approach_to_rob = 15
+tim_bottles_in_box = 12
+tim_bottles_used_at_60_meters_or_farther_percentage = 40
+tim_bottles_used_at_59_to_31_meters_percentage = 30
 
-class Tim:
-    def __init__(self, env):
-        self.env = env
-        self.bottles = 12
-        self.accuracy_at_30_meters = 90
-        self.accuracy_decrease_per_meter_above_30 = 5
-        self.accuracy_decrease_per_meter_below_25 = 7
-        self.closest_approach_to_tim = ROB_CLOSEST_APPROACH_TO_TIM
-        self.speed = 2
+# Initialize variables
+tim_accuracy = tim_accuracy_max
+tim_bottles_used = 0
+rob_distance_from_tim = tim_closest_approach_to_rob
 
-    def throw_bottles(self, rob):
-        # Calculate the number of bottles that Tim will throw
-        if rob.distance_from_tim >= 60:
-            bottles_thrown = int(0.4 * self.bottles)
-        elif rob.distance_from_tim < 31:
-            bottles_thrown = int(0.3 * self.bottles)
-        else:
-            bottles_thrown = int(self.bottles)
+# While Rob is within Tim's range
+while rob_distance_from_tim <= 100:
 
-        # Calculate the probability of Tim hitting Rob
-        probability_of_hit = self.accuracy_at_30_meters * (1 - (rob.distance_from_tim - 30) * self.accuracy_decrease_per_meter_above_30) * (1 - (30 - rob.distance_from_tim) * self.accuracy_decrease_per_meter_below_25)
-
-        # Throw the bottles
-        for bottle in range(bottles_thrown):
-
-            # Check if Rob is hit
-            hit = random.randint(0, 100) < probability_of_hit
-
-            # If Rob is hit, update the shield hits and bottles left
-            if hit:
-                rob.shield_hits += 1
-                self.bottles -= 1
-
-        # Update the distance between Rob and Tim
-        rob.distance_from_tim -= rob.speed
-
-
-class Rob:
-    def __init__(self, env):
-        self.env = env
-        self.shield_hits = 0
-        self.distance_from_tim = ROB_CLOSEST_APPROACH_TO_TIM
-        self.speed = 2
-
-    def walk(self):
-        while self.distance_from_tim > 0:
-            self.distance_from_tim -= self.speed
-
-    def pull_out_shield(self):
-        self.shield_hits = 0
-
-
-env = simpy.Environment()
-tim = Tim(env)
-rob = Rob(env)
-
-for i in range(100):
-    tim.throw_bottles(rob)
-    rob.walk()
-
-    if rob.shield_hits < 5:
-        print("Rob was hit in simulation {}.".format(i + 1))
+    # Calculate Tim's accuracy
+    if rob_distance_from_tim >= 60:
+        tim_accuracy = tim_accuracy_max - (rob_distance_from_tim - 60) * tim_accuracy_decrease_per_meter_100_to_30
+    elif rob_distance_from_tim < 30:
+        tim_accuracy = tim_accuracy_max - (30 - rob_distance_from_tim) * tim_accuracy_decrease_per_meter_25_to_0
     else:
-        print("Rob was not hit in simulation {}.".format(i + 1))
+        tim_accuracy = tim_accuracy_max
+
+    # Calculate the number of bottles Tim will throw
+    number_of_bottles_to_throw = random.randint(0, tim_bottles_in_box)
+
+    # Throw the bottles
+    for i in range(number_of_bottles_to_throw):
+        if random.random() <= tim_accuracy / 100:
+            print("Tim hit Rob!")
+        else:
+            print("Tim missed Rob!")
+
+    # Update Rob's distance from Tim
+    rob_distance_from_tim += 2
+
+# Print the number of bottles Tim used
+print("Tim used {} bottles.".format(tim_bottles_used))

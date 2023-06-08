b66c814a10ebfe00d624131772bbfffa4c9d0fd6
gabrielexposito13 <ge1316a@american.edu>
2023-06-07 22:02:37 -0400

    Update Accuracy Practice.py
    not yet working as intended.

--- Accuracy Practice.py
+++ Accuracy Practice.py
@@ -1,60 +1,70 @@
-import random
-import simpy
 import numpy as np
+import simpy
+import random
 
-NUM_SURVIVORS = 5 #objects
-ACCURACY = .03  #probability
-SHOTS_PER_MINUTE = 25 #per minute
-ZOMBIE_ATTACK_INTERVAL = 10 #per minute
+ROB_CLOSEST_APPROACH_TO_TIM = 101
 
-SIM_TIME = 120 #minutes
-
-zombies_killed = 0
-
-class Apocalypse:
-
-    def __init__(self, env, num_survivors, accuracy, shots_per_minute, zombie_attack_interval):
+class Tim:
+    def __init__(self, env):
         self.env = env
-        self.team = simpy.Resource(env, num_survivors)
-        self.accuracy = accuracy
-        self.shots_per_minute = shots_per_minute
-        self.zombie_attack_interval = zombie_attack_interval
+        self.bottles = 12
+        self.accuracy_at_30_meters = 90
+        self.accuracy_decrease_per_meter_above_30 = 5
+        self.accuracy_decrease_per_meter_below_25 = 7
+        self.closest_approach_to_tim = ROB_CLOSEST_APPROACH_TO_TIM
+        self.speed = 2
+
+    def throw_bottles(self, rob):
+        # Calculate the number of bottles that Tim will throw
+        if rob.distance_from_tim >= 60:
+            bottles_thrown = int(0.4 * self.bottles)
+        elif rob.distance_from_tim < 31:
+            bottles_thrown = int(0.3 * self.bottles)
+        else:
+            bottles_thrown = int(self.bottles)
+
+        # Calculate the probability of Tim hitting Rob
+        probability_of_hit = self.accuracy_at_30_meters * (1 - (rob.distance_from_tim - 30) * self.accuracy_decrease_per_meter_above_30) * (1 - (30 - rob.distance_from_tim) * self.accuracy_decrease_per_meter_below_25)
+
+        # Throw the bottles
+        for bottle in range(bottles_thrown):
+
+            # Check if Rob is hit
+            hit = random.randint(0, 100) < probability_of_hit
+
+            # If Rob is hit, update the shield hits and bottles left
+            if hit:
+                rob.shield_hits += 1
+                self.bottles -= 1
+
+        # Update the distance between Rob and Tim
+        rob.distance_from_tim -= rob.speed
 
 
-    def defense(self, zombie):
-        random_time = max(1, np.random.normal(self.shots_per_minute, 10))
-        yield self.env.timeout(random_time)
-        print(f"Dealt with {zombie} at {self.env.now:.2f}")
+class Rob:
+    def __init__(self, env):
+        self.env = env
+        self.shield_hits = 0
+        self.distance_from_tim = ROB_CLOSEST_APPROACH_TO_TIM
+        self.speed = 2
 
-def zombie(env, name, survivors,):
-    global zombies_killed
-    print(f"Zombie {name} appears at {env.now:.2f}!")
-    with survivors.team.request() as request:
-        yield request
-        print(f"Zombie {name} engaged at {env.now:.2f}!")
-        yield env.process(survivors.defense)
-        print(f"Zombie {name} killed at {env.now:.2f}!")
-        zombies_killed += 1
+    def walk(self):
+        while self.distance_from_tim > 0:
+            self.distance_from_tim -= self.speed
 
-def setup(env, num_survivors, shots_per_minute, accuracy, zombie_attack_interval):
-    survivors = Apocalypse(env, num_survivors, shots_per_minute, accuracy, zombie_attack_interval)
+    def pull_out_shield(self):
+        self.shield_hits = 0
 
-    for i in range(1, 6):
-        env.process(zombie(env, i, survivors))
-    
-    while True:
-        yield env.timeout(random.randint(zombie_attack_interval -1, zombie_attack_interval +1 ))
-        i += 1
-        env.process(zombie(env, num_survivors, survivors))
 
-print("Starting Apocalypse")
 env = simpy.Environment()
-env.process(setup(env, NUM_SURVIVORS, ACCURACY, SHOTS_PER_MINUTE, ZOMBIE_ATTACK_INTERVAL))
-env.run(until=SIM_TIME)
+tim = Tim(env)
+rob = Rob(env)
 
-print("Zombies Killed: "  + str(zombies_killed))
+for i in range(100):
+    tim.throw_bottles(rob)
+    rob.walk()
 
-
-
-
-
+    if rob.shield_hits < 5:
+        print("Rob was hit in simulation {}.".format(i + 1))
+    else:
+        print("Rob was not hit in simulation {}.".format(i + 1))
